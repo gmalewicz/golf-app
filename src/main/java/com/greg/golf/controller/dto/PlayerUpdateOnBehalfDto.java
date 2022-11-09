16eece5d1136525e6262bc9a00e8ec8ee45deb81
Grzegorz Malewicz <grzegorz.malewicz@gmail.com>
2022-11-09 10:30:24 +0100

    fixed smells

--- src/main/java/com/greg/golf/controller/dto/PlayerUpdateOnBehalfDto.java
+++ src/main/java/com/greg/golf/controller/dto/PlayerUpdateOnBehalfDto.java
@@ -1,6 +1,5 @@
 package com.greg.golf.controller.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
