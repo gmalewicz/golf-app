e0afe398611ea2c78eec203fd54260ba57db91de
Grzegorz Malewicz <grzegorz.malewicz@gmail.com>
2023-11-25 14:18:51 +0100

    bug fixea

--- src/main/java/com/greg/golf/captcha/AbstractCaptchaService.java
+++ src/main/java/com/greg/golf/captcha/AbstractCaptchaService.java
@@ -75,14 +75,8 @@ public abstract class AbstractCaptchaService implements ICaptchaService{
 
     protected String getClientIP() {
 
+        String xfHeader = request.getHeader("X-Forwarded-For");
 
-        String xfHeader;
-
-        if (allowedOrigins.equals(request.getServerName())) {
-            xfHeader = request.getHeader("X-Forwarded-For");
-        } else {
-            throw new ReCaptchaInvalidException("Attempt to validate recaptcha from unauthorized site");
-        }
         if (xfHeader == null) {
             return request.getRemoteAddr();
         }
